6b4bbd757c74d189b2bc5068c0fc9a0d6853dfc9
Mizuno Yuki <c6h4clch3@gmail.com>
2017-09-02 19:23:40 +0900

    modify: 現状の機能をvuex化

--- www/vue/store/modules/player.js
+++ www/vue/store/modules/player.js
@@ -10,32 +10,37 @@ export default {
   },
   actions: {
     initialize(context) {
-      name = Math.floor(Math.random() * 100) + 'さん';
-      systems = jsonp(
+      const name = Math.floor(Math.random() * 100) + 'さん';
+      context.commit('name', name);
+      let systems
+      jsonp(
         'https://www2.taruki.com/bcdice-api/v1/systems?callback=',
         null,
         function(err, data){
           if (err) {
             return context.state.systems;
           }
-          return data.systems;
+          systems = data.systems;
+          context.commit('initialize', {
+            name: name,
+            systems: systems,
+            selectedSystem: 'DiceBot'
+          });
         }
       );
-      context.commit('initialize', {
-        name: name,
-        systems: systems,
-        selectedSystem: 'DiceBot'
-      });
     },
     name(context, name) {
       context.commit('name', name);
+    },
+    selectedSystem(context, system) {
+      context.commit('selectedSystem', system);
     }
   },
   mutations: {
     initialize(state, initialParams) {
       state.name = initialParams.name;
       state.systems = initialParams.systems;
-      selectedSystem = initialParams.selectedSystem;
+      state.selectedSystem = initialParams.selectedSystem;
     },
     name(state, name) {
       state.name = name;
--- www/vue/store/store.js
+++ www/vue/store/store.js
@@ -1,15 +1,42 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import io from 'socket.io-client';
 
 import player from './modules/player';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {
-    socketio: {}
+  state(){
+    return {
+      socket: {},
+      room: ''
+    }
+  },
+  actions: {
+    room(context, room) {
+      context.commit('room', room);
+    }
+  },
+  mutations: {
+    room(state, room) {
+      if (typeof room !== 'string') {
+        return;
+      }
+      state.room = room;
+      if (typeof state.socket.disconnect === 'function') {
+        state.socket.disconnect();
+      }
+      if (room === '') {
+        state.socket = {};
+      } else {
+        state.socket = io.connect(`/room/${room}`);
+      }
+    }
   },
   modules: {
-    player
+    player: player
   }
 });
+
+export default store;
